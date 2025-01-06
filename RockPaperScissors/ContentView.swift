9743a1d2c55c975c990ab8fc9dc8ce299bb2c0dc
Marko Živanović <zivanovic.marko@icloud.com>
2025-01-06 20:51:05 +0100

    Initial commit

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -10,12 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            LinearGradient(colors: [.purple, .blue], startPoint: .top, endPoint: .bottom)
         }
-        .padding()
+        .ignoresSafeArea(.all)
     }
 }
 
