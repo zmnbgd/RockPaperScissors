a87e9354576097122fff7fafadffe45a31056e94
Marko Živanović <zivanovic.marko@icloud.com>
2025-01-07 22:18:49 +0100

    Commit 2.

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -11,16 +11,35 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var selectedGameOption = ["🪨", "✂️", "📄"].shuffled()
+    @State private var selectedGameOption = ["Rock", "Paper", "Scissors"].shuffled()
+    @State private var playerOneAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var score = 0
     
     var body: some View {
         ZStack {
             LinearGradient(colors: [.purple, .blue], startPoint: .top, endPoint: .bottom)
             VStack {
                 Text("Result is ...")
+                VStack {
+                    VStack {
+                        
+                    }
+                    ForEach(0..<2) { number in
+                        Button {
+                            appGameOption(number)
+                        } label: {
+                            Image(selectedGameOption[number])
+                        }
+                    }
+                }
             }
         }
         .ignoresSafeArea(.all)
+    }
+    
+    func appGameOption(_ number: Int) {
+        let option = selectedGameOption.randomElement()
     }
 }
 
