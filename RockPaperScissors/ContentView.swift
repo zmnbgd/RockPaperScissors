852034855f705b3f56be1552a0c40644e9589fc5
Marko Živanović <zivanovic.marko@icloud.com>
2025-01-08 19:48:53 +0100

    Commit 3.

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -10,37 +10,40 @@ import SwiftUI
 //MARK: Custom Game Text Modifier 
 
 struct ContentView: View {
-    
-    @State private var selectedGameOption = ["Rock", "Paper", "Scissors"].shuffled()
-    @State private var playerOneAnswer = Int.random(in: 0...2)
-    @State private var showingScore = false
+
+    @State private var gameOptions = ["Paper", "Rock", "Scissors"]
+    @State private var appChpoice = Int.random(in: 0...2)
+
     @State private var score = 0
+    @State private var showingScore = false
+    
+    @State private var playerChoice: Int?  = nil
+
     
     var body: some View {
         ZStack {
-            LinearGradient(colors: [.purple, .blue], startPoint: .top, endPoint: .bottom)
-            VStack {
-                Text("Result is ...")
+            LinearGradient(gradient: Gradient(colors: [.purple, .black]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            VStack(spacing: 20) {
+                Text("Rock, Paper, Scissors!")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .fontWeight(.black)
                 VStack {
-                    VStack {
-                        
-                    }
-                    ForEach(0..<2) { number in
-                        Button {
-                            appGameOption(number)
-                        } label: {
-                            Image(selectedGameOption[number])
-                        }
-                    }
+                    Text("App Choice is ... \(gameOptions[appChpoice])")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .fontWeight(.black)
+                    Image(gameOptions[appChpoice])
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 70, height: 70)
+                        .background(Color.clear)
                 }
             }
         }
-        .ignoresSafeArea(.all)
+        .edgesIgnoringSafeArea(.all)
     }
     
-    func appGameOption(_ number: Int) {
-        let option = selectedGameOption.randomElement()
-    }
 }
 
 #Preview {
