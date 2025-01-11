e17b6a6c02ffd92e28f6079279bcecc20ddfc516
Marko Živanović <zivanovic.marko@icloud.com>
2025-01-11 20:12:50 +0100

    Commit 6.

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -11,8 +11,8 @@ import SwiftUI
 
 struct ContentView: View {
 
-    @State private var gameOptions = ["Paper", "Rock", "Scissors"]
-    @State private var appChpoice = Int.random(in: 0...2)
+    @State private var moves = ["Paper", "Rock", "Scissors"]
+    @State private var appChpoice = Int.random(in: 0..<3)
 
     @State private var score = 0
     @State private var showingScore = false
@@ -28,16 +28,33 @@ struct ContentView: View {
                     .font(.largeTitle)
                     .foregroundColor(.white)
                     .fontWeight(.black)
-                VStack {
-                    Text("App Choice is ... \(gameOptions[appChpoice])")
+
+                VStack(spacing: 10) {
+                    Text("App Choice is ... \(moves[appChpoice])")
                         .font(.largeTitle)
                         .foregroundColor(.white)
                         .fontWeight(.black)
-                    Image(gameOptions[appChpoice])
+                    Image(moves[appChpoice])
                         .resizable()
                         .scaledToFit()
                         .frame(width: 70, height: 70)
-                        .background(Color.clear)
+                }
+                HStack(spacing: 20) {
+                     ForEach(0..<3) { number in
+                         Button {
+                             //MARK: Player move
+                         } label: {
+                             Text(moves[number])
+                                 .font(.system(size: 30))
+                                 .fontWeight(.bold)
+                                 .foregroundColor(.white)
+                                 .padding()
+                                 .frame(width: 100, height: 100)
+                                 .background(Color.blue.opacity(0.8))
+                                 .clipShape(Circle())
+                         }
+                        
+                    }
                 }
             }
         }
