8aa69d1783c2edc0984ae34521d335d437d99e85
Marko Živanović <zivanovic.marko@icloud.com>
2025-01-12 18:50:32 +0100

    Commit 7.

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 //MARK: Custom Game Text Modifier 
 
 struct ContentView: View {
-
-    @State private var moves = ["Paper", "Rock", "Scissors"]
+    
+    @State private var moves = ["rock", "paper", "scissors"]
     @State private var appChpoice = Int.random(in: 0..<3)
 
     @State private var score = 0
@@ -21,44 +21,45 @@ struct ContentView: View {
 
     
     var body: some View {
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [.purple, .black]), startPoint: .topLeading, endPoint: .bottomTrailing)
-            VStack(spacing: 20) {
-                Text("Rock, Paper, Scissors!")
-                    .font(.largeTitle)
-                    .foregroundColor(.white)
-                    .fontWeight(.black)
-
-                VStack(spacing: 10) {
-                    Text("App Choice is ... \(moves[appChpoice])")
-                        .font(.largeTitle)
-                        .foregroundColor(.white)
-                        .fontWeight(.black)
-                    Image(moves[appChpoice])
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 70, height: 70)
-                }
-                HStack(spacing: 20) {
-                     ForEach(0..<3) { number in
-                         Button {
-                             //MARK: Player move
-                         } label: {
-                             Text(moves[number])
-                                 .font(.system(size: 30))
-                                 .fontWeight(.bold)
-                                 .foregroundColor(.white)
-                                 .padding()
-                                 .frame(width: 100, height: 100)
-                                 .background(Color.blue.opacity(0.8))
-                                 .clipShape(Circle())
-                         }
-                        
+        NavigationStack {
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [.purple, .black]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                VStack {
+                    VStack(spacing: 10) {
+                        Text("App Choice is ... \(moves[appChpoice])")
+                            .font(.title3)
+                            .foregroundColor(.black)
+                            .fontWeight(.black)
+                        Image(moves[appChpoice])
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 70, height: 70)
+                    }
+                    HStack(spacing: 20) {
+                        ForEach(0..<3) { number in
+                            Button {
+                                //MARK: Player move
+                            } label: {
+                                Image(moves[number])
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50, height: 50)
+                                    .padding()
+                                    .frame(width: 100, height: 100)
+                                    .background(Color.black)
+                                    .clipShape(Circle())
+                                
+                                
+                            }
+                            
+                        }
                     }
                 }
             }
+            .navigationTitle("Rock Paper Scissors")
+            .edgesIgnoringSafeArea(.all)
         }
-        .edgesIgnoringSafeArea(.all)
+
     }
     
 }
