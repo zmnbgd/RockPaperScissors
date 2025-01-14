26e7e450ca871ee83f4c7af963a36063c90f0f13
Marko Živanović <zivanovic.marko@icloud.com>
2025-01-14 19:59:11 +0100

    Commit 9.

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -28,57 +28,60 @@ extension View {
 
 struct ContentView: View {
     
-    @State private var moves = ["rock", "paper", "scissors"]
-    @State private var appChpoice = Int.random(in: 0..<3)
-
+    @State private var moves = ["✊🏼", "🤚🏼", "✌🏼"]
+    @State private var appMove = Int.random(in: 0...2)
+    @State private var playerMove: Int?
     @State private var score = 0
-    @State private var showingScore = false
     
-    @State private var playerChoice: Int?  = nil
-
     
     var body: some View {
-        NavigationStack {
-            ZStack {
-                LinearGradient(gradient: Gradient(colors: [.purple, .black]), startPoint: .topLeading, endPoint: .bottomTrailing)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.black.opacity(0.4), .black]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            VStack {
+                Text("Rock Paper Scissors")
+                    .font(.largeTitle).bold()
+                    .foregroundColor(.white)
                 VStack {
-                    VStack(spacing: 10) {
-//                        Text("App Choice is ... \(moves[appChpoice])")
-                        appTextModifier(text: "App Choice is ... \(moves[appChpoice])")
-                            .font(.title3)
-                            .foregroundColor(.black)
-                            .fontWeight(.black)
-                        Image(moves[appChpoice])
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 70, height: 70)
+                    VStack(spacing: 30) {
+                        Spacer()
+                        Text("App move is ...?")
+                            .font(.largeTitle).bold()
+                            .foregroundColor(.white)
+                        Text(moves[appMove])
+                            .font(.largeTitle)
                     }
-                    HStack(spacing: 20) {
-                        ForEach(0..<3) { number in
-                            Button {
-                                //MARK: Player move
-                            } label: {
-                                Image(moves[number])
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 50, height: 50)
-                                    .padding()
-                                    .frame(width: 100, height: 100)
-                                    .background(Color.black)
-                                    .clipShape(Circle()) 
-                            }
+                    ForEach(0..<3) { number in
+                        Button {
+                            //MARK: Selected move
+                        } label: {
+                            Image(moves[number])
                         }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.ultraThinMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                
+                Spacer()
             }
-            .navigationTitle("Rock Paper Scissors")
-            .edgesIgnoringSafeArea(.all)
+            .padding()
         }
-
     }
-    
 }
 
 #Preview {
     ContentView()
 }
+
+
+
+
+
